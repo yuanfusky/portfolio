5bd5d70a2660e1d744bb91a4854a9a5c5dca3b74
Ren Jingyuan <realrenjingyuan@gmail.com>
2026-09-16 20:48:38 +0200

    Add documentary visual works

---
+++ src/data/visualWorks.ts
@@ -0,0 +1,43 @@
+export type VisualWork = {
+  titleZh: string;
+  titleEn: string;
+  platform: string;
+  role: string;
+  image: string;
+  href: string;
+};
+
+export const visualWorks: VisualWork[] = [
+  {
+    titleZh: '秃然发生',
+    titleEn: 'Oh! My Hair',
+    platform: 'Tencent News',
+    role: 'Director',
+    image: '/images/visual-notes/oh-my-hair.webp',
+    href: 'https://v.qq.com/x/cover/mzc00200y9qhm0d/g3331usuhoo.html?ptag=mzhan',
+  },
+  {
+    titleZh: '十三邀 第八季',
+    titleEn: 'Thirteen Talks · Season 8',
+    platform: 'Tencent Video',
+    role: 'Episode 2 Director',
+    image: '/images/visual-notes/thirteen-talks.webp',
+    href: 'https://www.youtube.com/watch?v=AUNLMTla3o0',
+  },
+  {
+    titleZh: '籍籍无名',
+    titleEn: 'Nobody but Somebody',
+    platform: 'Shanghai Television',
+    role: 'Episode 4 Director',
+    image: '/images/visual-notes/nobody-but-somebody.webp',
+    href: 'https://www.bilibili.com/bangumi/play/ss121882',
+  },
+  {
+    titleZh: '田野之上',
+    titleEn: 'Life in the Fields',
+    platform: 'Shanghai Television',
+    role: 'Assistant Director',
+    image: '/images/visual-notes/life-in-the-fields.webp',
+    href: 'https://www.youtube.com/watch?v=TiuZwfoPYVM',
+  },
+];
